fd673494ef79f04214abea096cd7b27f364d1394
lalekosucu <lalekosucu@gmail.com>
2025-12-27 14:52:22 +0100

    changes has reverted

--- llava/model/builder.py
+++ llava/model/builder.py
@@ -23,7 +23,7 @@ from llava.model import *
 from llava.constants import DEFAULT_IMAGE_PATCH_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
 
 
-def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, load_4bit=False, device_map="cpu", device="cuda"):
+def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, load_4bit=False, device_map="gpu", device="cuda"):
     kwargs = {"device_map": device_map}
 
     if load_8bit:
@@ -102,8 +102,6 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
                 tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False)
                 model = LlavaLlamaForCausalLM.from_pretrained(
                     model_path, 
-                    low_cpu_mem_usage=True,
-                    offload_folder=os.environ.get("LLAVA_OFFLOAD_DIR", "/Volumes/T7/hf_offload"),
                     **kwargs
                 )
     else:
